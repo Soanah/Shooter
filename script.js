79ccacf22d66573af1c410d8f4b63d6479444e42
Soanah <morgane.lapisardi@hetic.net>
2018-11-05 16:35:20 +0100

    :tada: Init

---
+++ script.js
@@ -0,0 +1,12 @@
+let pets = new Set(["Cat", "Dog", "Hamster"]);
+pets["species"] = "mammals";
+
+for (let pet in pets) {
+   console.log(pet); // "species"
+}
+
+for (let pet of pets) {
+    console.log(pet); // "Cat", "Dog", "Hamster"
+}
+
+console.log(pets)
